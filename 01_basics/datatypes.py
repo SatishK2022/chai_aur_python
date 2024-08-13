386a4aa0d9f0f3cc5f527ae1885f4dd107274746
SatishK2022 <sk7399052@gmail.com>
2024-08-13 12:05:00 +0530

    add datatypes

---
+++ 01_basics/datatypes.py
@@ -0,0 +1,20 @@
+# Object Types / Data Types
+
+"""
+# Data Types
+1) Number -> 1234, 3.14, 3+4j, 0b111, Decimal(), Fraction()
+2) String -> 'spam', "Satish", b'a\x01c', u'sp\xc4m'
+3) List -> [1, 2, 3], [1, [2, "three"], 4.5], list(range(10))
+4) Tuple -> (1, 'spam', 4, 'U'), tuple('spam'), namedtuple
+5) Dictionary -> {'a': 1, 'b': 2}, dict(zip(['a', 'b'], [1, 2]))
+
+# Set -> set('abc'), {'a', 'b', 'c'}
+
+# File -> open('spam.txt', 'w')
+
+# Boolean -> True, False
+# None -> None
+# Functions, modulel, classes
+
+# Advance Types -> Decorators, Generators, Iterators, MetaProgramming
+"""
